380fff909de35daaa53d8d8f21701350de5e5d52
unknown <nishonovamuattar39@gmail.com>
2024-08-15 18:06:04 +0500

    models created

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib.admin import ModelAdmin,register,StackedInline
-from .models import Category,Product_image,Product_image
+from .models import Category,Product_model,Product_image
 # Register your models here.
 
 
@@ -7,4 +7,12 @@ class Product_imageStackedInline(StackedInline):
     model = Product_image
     fields = ('image','product')
 
+@register(Product_model)
+class Product_modelModelAdmin(ModelAdmin):
+    # inlines = [Product_imageStackedInline, ]
+    # inlines = (Product_imageStackedInline, )
+    inlines = Product_imageStackedInline, 
 
+@register(Category)
+class CategoryModelAdmin(ModelAdmin):
+    pass
