9636fe5e397c930305c7b1c6d014718ef509ef60
unknown <nishonovamuattar39@gmail.com>
2024-08-15 17:57:25 +0500

    models created

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
-
+from django.contrib.admin import ModelAdmin,register,StackedInline
+from .models import Category,Product_image,Product_image
 # Register your models here.
+
+
+class Product_imageStackedInline(StackedInline):
+    model = Product_image
+    fields = ('image','product')
+
+
--- products/models.py
+++ products/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+from django.db.models import Model,CharField,TextField,PositiveIntegerField,ImageField,ForeignKey,CASCADE,FloatField
 
 # Create your models here.
+
+class Category(Model):
+    name = CharField(max_length=255)
+    parent = ForeignKey("self",on_delete=CASCADE,null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+class Product_model(Model):
+    name = CharField(max_length=1000)
+    price = FloatField()
+    discount = PositiveIntegerField()
+    colour = CharField(max_length=200, null=True, blank=True)
+    short_description = TextField()
+    description = TextField()
+    quantity = PositiveIntegerField(default=1)
+    category = ForeignKey(Category,on_delete=CASCADE,related_name='products')
+
+    def __str__(self) -> str:
+        return   f"{self.name}"
+    
+    
+    
+
+class Product_image(Model):
+    image = ImageField(upload_to='products')
+    product = ForeignKey(Product_model,on_delete=CASCADE,related_name = 'images')   
