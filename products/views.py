5ea93600e80c639c53abe7554363b55b5814b0eb
unknown <nishonovamuattar39@gmail.com>
2024-09-05 18:27:08 +0500

    smth uploaded

--- products/views.py
+++ products/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from .models import Product_model,Cart,CartItem
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import get_list_or_404
+from django.shortcuts import get_object_or_404
 
 
 
@@ -23,16 +23,27 @@ def product_detail(request,id):
 
 def add_to_card(request):
     if request.method == 'POST':
-        print(request.POST)
-        data = json.loads(request.POST)
+        data = json.loads(request.body)
+        print(data)
         product_id = data.get('product_id')
         quantity = data.get('quantity')
 
-        product = get_list_or_404(Product_model, id= product_id)
+        product = get_object_or_404(Product_model, id= product_id)
         # print(request.user)
+        # product = Product_model.objects.get(id = product_id,)
         cart , created = Cart.objects.get_or_create(user=request.user, is_active=True)
 
-        cart_item , created = CartItem.objects.get_or_create(cart=cart, product=product)
+        cart_item , item_created = CartItem.objects.get_or_create(cart=cart, product=product)
 
-    return JsonResponse(data={'status':'Okey'})
+        if not item_created:
+            cart_item.quantity += 1
+        else:
+            cart_item.quantity = 1
+
+
+        cart_item.save()
+        return JsonResponse(data={'status':'okey'})
+        
+
+    return JsonResponse(data={'status':'error'})
 
