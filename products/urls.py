d7ff6f6b9a0b8ba0e5713ac6a84eb5a5cfc9258a
unknown <nishonovamuattar39@gmail.com>
2024-09-18 18:34:38 +0500

    new updates

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path 
-from .views import Product_views,product_detail,add_to_card,user_cart
+from .views import Product_views,product_detail,add_to_card,user_cart,delete_cart
 
 urlpatterns = [
     path('',Product_views, name="product_urls"),
     path('product/<int:id>/',product_detail, name='product-detail'),
     path('add_to_card/',add_to_card, name='add_to_card'),
-    path('user-cart/', user_cart,name='user-cart')
+    path('user-cart/', user_cart,name='user-cart'),
+    path('delete-cart/',delete_cart,name='delete-cart')
 
 ]
